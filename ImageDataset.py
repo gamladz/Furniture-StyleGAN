b6e637d743e73e08f87e4188b4e91cbbc1efad6c
Harry Berg <ice@Harrys-MacBook-Pro.local>
2021-03-22 23:58:04 +0000

    added tqdm progress bar and fixed bug looking for image in wrong s3 path

--- ImageDataset.py
+++ ImageDataset.py
@@ -10,6 +10,7 @@ import progressbar
 import json
 import requests
 from pathlib import Path
+from tqdm import tqdm
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -86,59 +87,20 @@ class ImageDataset(torch.utils.data.Dataset):
     def download(self, root, BUCKET_NAME):
 
         with open('DATA/data.json') as f:
-            data = json.load(f)
+            data = json.load(f) # read data containing image paths
 
-        paths = ('/'.join(path.split('/')[1:]) for category in data.values() for item in category.values() for path in item['images'])
+        paths = ('/'.join(path.split('/')[1:]) for category in data.values() for item in category.values() for path in item['images']) # generate paths
 
-        for path in paths:
+        for path in tqdm(paths):
             # OS FRIENDLY WAYS TO GET THE IMG PATH AND DIR
             fp = os.path.join(self.root_dir, *os.path.split(path))
             if os.path.exists(fp):
                 continue
             dir = os.path.join(*os.path.split(fp)[:1])
             Path(dir).mkdir(parents=True, exist_ok=True) # create dir if doesnt exist
-            response = requests.get(f'https://ikea-dataset.s3.us-east-2.amazonaws.com/{path}')
+            response = requests.get(f'https://ikea-dataset.s3.us-east-2.amazonaws.com/data/{path}')
             with open(fp, 'wb') as f:
                 f.write(response.content)
-
-        # Check the size of the dataset
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(BUCKET_NAME)
-        size = sum(1 for _ in bucket.objects.all())
-
-        # Create a paginator object, so it ignores the 1000 limit
-        client = boto3.client('s3')
-        # Create a reusable Paginator
-        paginator = client.get_paginator('list_objects')
-        # Create a PageIterator from the Paginator
-        # The Prefix='data' parameter ensures that we are only taking 
-        # the images from the data folder
-        page_iterator = paginator.paginate(Bucket=BUCKET_NAME,
-                                        Prefix='data/')
-
-        # Create a progress bar, so it tells how much is left
-        print('Downloading...')
-        bar = progressbar.ProgressBar(
-                maxval=size,
-                widgets=[progressbar.Bar('=', '[', ']'),
-                        ' ', progressbar.Percentage()])
-        bar.start()
-        r = 0
-
-        # Start the download
-        for page in page_iterator:
-            for content in page['Contents']:
-                # Create a directory for each type of furniture ('bin', 'cookware'...)
-                os.makedirs(f"{root}/{content['Key'].split('/')[1]}", exist_ok=True)
-                LOCAL_FILE_NAME = (f"{root}/{content['Key'].split('/')[1]}"
-                                + f"/{content['Key'].split('/')[-1]}")
-                if not os.path.exists(LOCAL_FILE_NAME):
-                    client.download_file(BUCKET_NAME, content['Key'], LOCAL_FILE_NAME)
-                
-                # Update the progress bar
-                bar.update(r + 1)
-                r += 1
-        bar.finish()
 
 def split_train_test(dataset, train_percentage):
     train_split = int(len(dataset) * train_percentage)
